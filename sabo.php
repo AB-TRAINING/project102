38b6eb689a233acf9677451b25e7cf5b283bc64b
alexis <alexissaborrido.ece@gmail.com>
2021-03-11 12:09:36 +0800

    added sabo

---
+++ sabo.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> HELLO, THIS IS TASK 2</title>
+        <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+        <style>
+            h1 {
+                color:whitesmoke;
+                font-family: 'Righteous', cursive;
+            }
+
+            p{
+                color:whitesmoke;
+                font-family: 'Righteous', cursive;
+            }
+
+
+            body{
+                background-image: url("https://www.pcclean.io/wp-content/uploads/2020/4/rU32uy.jpg");
+                background-repeat: no-repeat;
+                background-size: 100%;
+            }
+
+            #images img{
+                margin-left: auto;
+                margin-right: auto;
+                display: block;
+                border: rebeccapurple 10px;
+            }
+
+        </style>
+    </head>
+
+    <body>
+        <h1 style="text-align: center;">HELLO, CALL ME SABO</h1>
+        <p style="text-align: center;">I am Alexis Saborrido. A noob dev. Currently training to be a better one.
+            I am not confident with my programming skills,
+            but I am very eager to learn more in every aspect.</p>
+        <p style="text-align: center;">Email me here: alexissaborrido.ece@gmail.com</p>
+            <p style="text-align: center;"><a href="/">Back Home</a></p>
+
+        <div id="images">
+            <img src="https://digitalsynopsis.com/wp-content/uploads/2015/03/web-designer-developer-jokes-humour-funny-14.jpg" width="250" height="450" style="text-align: center;">
+        </div>
+
+    </body>
+</html>
