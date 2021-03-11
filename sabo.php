f2ce4f5f79572a385b48755f8ce9285dc9dda04b
alexis <alexissaborrido.ece@gmail.com>
2021-03-11 18:38:08 +0800

    edited about page

--- sabo.php
+++ sabo.php
@@ -1,47 +1,111 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title> HELLO, THIS IS TASK 2</title>
-        <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
-        <style>
-            h1 {
-                color:whitesmoke;
-                font-family: 'Righteous', cursive;
-            }
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>TASK 2</title>
 
-            p{
-                color:whitesmoke;
-                font-family: 'Righteous', cursive;
-            }
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.6/examples/cover/">
+
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+    <style>
+        h1 {
+            color:whitesmoke;
+            font-family: 'Righteous', cursive;
+        }
+        h3 {
+            color:whitesmoke;
+            font-family: 'Righteous', cursive;
+        }
+        p{
+            color:whitesmoke;
+            font-family: 'Righteous', cursive;
+        }
+
+        a{
+            color:blue;
+            font-family: 'Righteous', cursive;
+        }
+
+        body{
+            background-image: url("/assets/image/sabo-bg.jpg");
+            background-repeat: no-repeat;
+            background-size: 100%;
+        }
+        #images img{
+            margin-left: auto;
+            margin-right: auto;
+            display: block;
+            border: rebeccapurple 10px;
+        }
+
+    </style>
 
 
-            body{
-                background-image: url("/assets/image/sabo-bg.jpg");
-                background-repeat: no-repeat;
-                background-size: 100%;
-            }
 
-            #images img{
-                margin-left: auto;
-                margin-right: auto;
-                display: block;
-                border: rebeccapurple 10px;
-            }
 
-        </style>
-    </head>
 
-    <body>
-        <h1 style="text-align: center;">HELLO, CALL ME SABO</h1>
-        <p style="text-align: center;">I am Alexis Saborrido. A noob dev. Currently training to be a better one.
+    <style>
+      
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="/cover.css" rel="stylesheet">
+  </head>
+  
+  <body class="d-flex h-100 text-center text-white bg-dark">
+    
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header class="mb-auto">
+        <div>
+          <h3 class="float-md-start mb-0">AB-Training</h3>
+          <nav class="nav nav-masthead justify-content-center float-md-end">
+          </nav>
+        </div>
+      </header>
+    
+      <main class="px-3">
+        <img src="/assets/image/sabo-profile.jpg" style="width: 150px; height: 150px;">
+        <h1>Hello there, call me Sabo</h1>
+        <p class="lead">I am Alexis Saborrido. A noob dev. Currently training to be a better one.
             I am not confident with my programming skills,
             but I am very eager to learn more in every aspect.</p>
-        <p style="text-align: center;">Email me here: alexissaborrido.ece@gmail.com</p>
-            <p style="text-align: center;"><a href="/">Back Home</a></p>
-
-        <div id="images">
-            <img src="/assets/image/sabo-meme.jpg" width="250" height="450" style="text-align: center;">
-        </div>
-
+        <p class="lead">
+          <a href="/index.php" class="btn btn-lg btn-secondary fw-bold border-white bg-blue">Home</a>
+        </p>
+      </main>
+    
+      <footer class="mt-auto text-white-50">
+        <p>Visit me on facebook <a href="https://twitter.com/mdo" class="text-blue">here</a>.</p>
+      </footer>
+    </div>
+    
+    
+        
+      
+    
     </body>
+
+    
 </html>
