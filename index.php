8e8fc8044b971cda792fa5f4b32fa67bdc04ecca
Romski19 <romeoramosa@gmail.com>
2021-03-11 18:40:19 +0700

    added link to romeo's profile

--- index.php
+++ index.php
@@ -109,6 +109,20 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="col-md-4">
+                            <div class="card mb-4 box-shadow">
+                                <img class="card-img-top" src="/assets/image/romeo-tmb.png" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Romeo Ramosa</h5>
+                                    <p class="card-text">Qucik Setup for Romeo Ramosa</p>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="btn-group">
+                                            <a href="/pm.php" type="button" class="btn btn-sm btn-outline-secondary">View</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>  
 
                     </div>
                 </div>
